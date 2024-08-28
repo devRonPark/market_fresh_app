e83bf5313655908209184be8943873fe518ffa02
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-08-28 19:33:05 +0900

    feat: 상품 상세정보 페이지 마크업 및 스타일링, 상품 상세정보 조회 기능 구현

--- src/main/java/com/market/controller/ProductController.java
+++ src/main/java/com/market/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,4 +20,10 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 	private final ProductService productService;
 
+	@GetMapping("/{productId}")
+	public String productDetailPage(@PathVariable("productId") Long productId, Model model) throws Exception {
+		Product product = productService.getProductById(productId);
+		model.addAttribute("product", product);
+		return "productDetail";
+	}
 }
